ce6726d5ddcb88ceb8f0781ab0883fc892234eb7
dathdka <dathdka@gmail.com>
2022-11-25 21:18:49 +0700

    upload local file

--- scraping/actions/propertiesHandler/imageHandler.js
+++ scraping/actions/propertiesHandler/imageHandler.js
@@ -1,18 +1,27 @@
 const axios = require("axios");
-
-const fetchImage = (imgLink) => new Promise(async (resolve, reject) => {
-    let res = await axios.get(imgLink)
-    if(res){
-        console.log('ok')
-        resolve();
-    }
-    else{
-        console.log('not ok')
-        resolve();
-    }
-});
+const firebase = require("../../../firebase");
+const fetchImage = (imgLink) =>
+  new Promise(async (resolve, reject) => {
+    await axios
+      .get(imgLink)
+      .then((res) => {
+        if (res?.data) resolve(res.data);
+        else resolve(null);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  });
 
 const imageHandler = async (item, imgLink) => {
-    await fetchImage(imgLink)
+  try {
+    var img = await fetchImage(imgLink);
+    firebase.file(new Date())
+    await firebase.upload('test/hello.txt')
+    console.log(await firebase.getFiles())
+    // firebase.upload(new Date(),blobFile)
+  } catch (error) {
+    console.error(error);
+  }
 };
 module.exports = imageHandler;
